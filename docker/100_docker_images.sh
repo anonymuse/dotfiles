aa0622542696c0869b15c1ccad00b7ca61dc422b
Jesse White <dev.white.jesse@dev.bwater.com>
2016-03-14 10:14:29 -0400

    Add skeleton Docker image install script

---
+++ docker/100_docker_images.sh
@@ -0,0 +1,22 @@
+# Install Docker images that we like to have local
+
+images=(
+  postgres
+  nginx
+  busybox
+  alpine
+  java:7
+  java:8
+  java:jdk
+  anapsix/alpine-java:7
+  anapsix/alpine-java:8
+  anapsix/alpine-java:jdk
+  ubuntu
+  redis
+  debian
+  mysql
+)
+
+#TODO: function to check if images are installed
+
+#TODO: function to install all above images
