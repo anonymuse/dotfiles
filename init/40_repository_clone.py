de68714772be98d8cc5817b1e82f9cd87c49d9cd
Jesse White <anonymuse@gmail.com>
2016-04-02 11:24:10 -0400

    Add regex function to repository clone
      Let's also use a Python program to accomplish this instead of bash.
      We've scoped out what we have to do in the program in this commit as
      well.

---
+++ init/40_repository_clone.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+#
+###################################
+#  Parlay configuration utility   #
+##########################################################
+#                                                        #
+# Objective:                                             #
+# Clone the desired repositories for a fresh OS install  #
+#                                                        #
+# Learn:                                                 #
+#                                                        #
+#                                                        #
+#                                                        #
+##########################################################
+
+import os
+import re
+
+my_repos = '/Users/jesse/Code/my_repos'
+pub_repos = '/Users/jesse/Code/public_repos'
+
+# Set up Vundle, which has a special install
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Clone public repositories
+#git clone https://github.com/pallets/flask.git
+
+# Do I want to put the full path of the repo here, or just the name? Not sure
+clones = [
+        'https://github.com/pallets/flask.git',
+        'https://github.com/cptactionhank/docker-atlassian-confluence.git',
+        'https://github.com/spf13/hugoThemes.git',
+        'https://github.com/braydie/HowToBeAProgrammer.git',
+        'https://github.com/mizzy/serverspec.git',
+        'https://github.com/behave/behave.git',
+        ]
+
+# Clone my repositories
+#TODO: Create Repo cloner function
+# Extracts the clone folder path from the clone link
+# Takes a list of repos and clones ones that don't exist
+# Skips ones that do exist
+# Non-loop example:
+repo = 'https://github.com/behave/behave.git'
+
+#TODO: Create repository scan regex
+clone_regex = re.compile('https:\/\/github.com\/\w+\/(.*).git$')
+result = clone_regex.match(repo)
+repo_path = result.group(1)
+print(repo_path)
+
+#TODO: Get the folder name from the https link
+# https://github.com/pallets/flask.git == 'flask'
+
+# If the folder existst already, move on to the next file
+
+# If the folder doesn't exist, clone the link into the named folder
