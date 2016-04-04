ec05389d975cf89b29e9a91211bfe3f5626596ae
Jesse White <anonymuse@gmail.com>
2016-04-03 20:56:47 -0400

    Separate out Vundle install

---
+++ 50_vundle_install.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#
+###################################
+#  Parlay configuration utility   #
+##########################################################
+#                                                        #
+# Objective:                                             #
+# Clone the desired repositories for a fresh OS install  #
+#                                                        #
+# Learn:                                                 #
+#                                                        #
+#                                                        #
+#                                                        #
+##########################################################
+
+# Set up Vundle, which has a special install
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
