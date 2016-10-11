be80abeee78cdf780b63af085e1421d6d1978912
Jesse White <anonymuse@gmail.com>
2016-10-10 21:52:39 -0400

    Add script for package update

---
+++ homebrew/brew_update.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Update brew, brew cask and clean up
+(set -x; brew update;)
+(set -x; brew cleanup;)
+(set -x; brew cask cleanup;)
+
+# Set some colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+# Set up the casks list as a variable
+casks=( $(brew cask list) )
+
+for cask in ${casks[@]}
+do
+    # Check for the newest version(s)
+    version=$(brew cask info $cask | sed -n "s/$cask:\ \(.*\)/\1/p")
+    installed=$(find "/usr/local/Caskroom/$cask" -type d -maxdepth 1 -maxdepth 1 -name "$version")
+
+    if [[ -z $installed ]]; then
+        # Install a new version, and remove old versions
+        echo "${red}${cask}${reset} requires ${red}update${reset}."
+        (set -x; brew cask uninstall $cask --force;)
+        (set -x; brew cask install $cask --force;)
+    else
+        echo "${red}${cask}${reset} is ${green}up-to-date${reset}."
+    fi
+done
+
