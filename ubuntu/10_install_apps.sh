9436aed4d5ece4b7220b170dd59cfd0f8efc2e36
Jesse White <anonymuse@gmail.com>
2017-04-14 16:56:16 -0400

    Update Docker install

--- ubuntu/10_install_apps.sh
+++ ubuntu/10_install_apps.sh
@@ -12,4 +12,35 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
 
+# Programs
+
+## Docker
+## Allow apt to use repositories over HTTPS
+sudo apt install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  software-properties-common
+
+## Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+## Add the Docker repository
+## Note: The lsb_release -cs sub-command below returns the name of your
+## Ubuntu distribution, such as xenial.
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+sudo apt update -y
+
+## Check where your package will come from and install
+apt-cache policy docker-engine
+sudo apt install docker-ce
+
+# Ensure that Docker runs at startup
+sudo systemctl enable docker
+
+# Set up sudo-less Docker
+sudo usermod -aG docker $(whoami)
 
