bf3399400f5ad92b2adf4ca57ed1bc7e35ece320
Jesse White <anonymuse@gmail.com>
2017-04-14 15:42:56 -0400

    Add Ubuntu configuration script

---
+++ ubuntu/10_install_apps.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Ubuntu customization script
+
+# Text
+
+## ViM
+sudo apt install vim
+
+## Sublime Text 3
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+
+
