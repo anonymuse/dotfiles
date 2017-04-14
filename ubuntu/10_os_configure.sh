f46279b879dcb2d1c6e88395eccb452a01292f6d
Jesse White <anonymuse@gmail.com>
2017-04-14 16:58:43 -0400

    Add OS configuration script

---
+++ ubuntu/10_os_configure.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# Ubuntu customization script
+# OS Configuration
+
+# Set 9 workspaces
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 3
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 3
