014993e490b6ec98f1d4bd3cf0c4edf5d65edba4
Jesse White <anonymuse@gmail.com>
2017-08-12 09:35:27 -0400

    Set formatting on application install script

--- ubuntu/30_install_apps.sh
+++ ubuntu/30_install_apps.sh
@@ -2,39 +2,52 @@
 #
 # Ubuntu customization script
 
-# Text
+###############################################
+##                 Text                      ##
+###############################################
 
-## ViM
+######################################
+# Install ViM                        #
+######################################
+
 sudo apt install vim
 
-## Sublime Text 3
+######################################
+# Install Sublime Text 3             #
+######################################
+
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
 sudo apt-get install sublime-text-installer
 
-# Programs
+###############################################
+##                Containers                 ##
+###############################################
 
-## Docker
-## Allow apt to use repositories over HTTPS
+######################################
+# Install Docker                     #
+######################################
+
+# Allow apt to use repositories over HTTPS
 sudo apt install -y \
   apt-transport-https \
   ca-certificates \
   curl \
   software-properties-common
 
-## Add Docker's official GPG key
+# Add Docker's official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-## Add the Docker repository
-## Note: The lsb_release -cs sub-command below returns the name of your
-## Ubuntu distribution, such as xenial.
+# Add the Docker repository
+# Note: The lsb_release -cs sub-command below returns the name of your
+# Ubuntu distribution, such as xenial.
 sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
 sudo apt update -y
 
-## Check where your package will come from and install
+# Check where your package will come from and install
 apt-cache policy docker-engine
 sudo apt install docker-ce
 
@@ -44,7 +57,12 @@ sudo systemctl enable docker
 # Set up sudo-less Docker
 sudo usermod -aG docker $(whoami)
 
+###############################################
+##                  Input                    ##
+###############################################
 
-# Install touchpad utilities
+######################################
+# Install Touchpad Utilities         #
+######################################
 # See https://github.com/advancingu/XPS13Linux/issues/3
 sudo apt install -y xserver-xorg-input-libinput
