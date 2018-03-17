a996e1b39b252c3e28de3d568823dc911d1008b5
Jesse White <anonymuse@gmail.com>
2018-03-17 12:59:40 -0400

    Cleanup sublime, vivaldi installers

--- ubuntu/20_install_repos.sh
+++ ubuntu/20_install_repos.sh
@@ -7,19 +7,14 @@
 ##                External                   ##
 ###############################################
 
-# Install the Vivaldi repository
+# Vivald
 echo "echo deb http://repo.vivaldi.com/stable/deb/ stable main > /etc/apt/sources.list.d/vivaldi.list" | sudo sh
 curl http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key add -
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1397BC53640DB551
-sudo apt-get update
 
-# Install the Sublime Text Repository
-# Install the GPG key
+# Sublime Text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-# Set the channel
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-# Update and install
-sudo apt update && sudo apt install sublime-text
 
 # Asciinema
 sudo apt-add-repository --yes ppa:zanchey/asciinema
--- ubuntu/30_install_apps.sh
+++ ubuntu/30_install_apps.sh
@@ -16,9 +16,7 @@ sudo apt install vim
 # Sublime Text 3                     #
 ######################################
 
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
-sudo apt-get update
-sudo apt-get install sublime-text-installer
+sudo apt install sublime-text-installer
 
 ###############################################
 ##                Containers                 ##
