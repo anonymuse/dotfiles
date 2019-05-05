2a168f7cfca5b67f6cd21252136c7da53745f850
Jesse White <anonymuse@gmail.com>
2019-05-05 14:47:38 -0500

    Add background, additional utilities for Bluetooth troubleshooting

--- ubuntu/scripts/fix_bluetooth.sh
+++ ubuntu/scripts/fix_bluetooth.sh
@@ -3,8 +3,16 @@
 # Based on experiences with Bluetooth mice, it appears that a small fix is
 # required in order to pair devices reliably.
 # Reference: https://ericasberry.com/blog/2016/09/30/pairing-a-logitech-mx-master-mouse-with-ubuntu-16-04-using-bluetooth/
+# https://stackoverflow.com/questions/22062037/hcitool-lescan-shows-i-o-error/23059924
 # This may be deprecated in later version of Ubuntu
 
 sudo hciconfig hci0 sspmode 1
-sudo ciconfig hci0 down
+sudo hciconfig hci0 down
 sudo hciconfig hci0 up
+
+# Can use additional CLI commands to check for the MAC address of the mouse with
+# '$ hcitool lescan' after which you can add it with 'gatttool'. You may need
+# to run the hciconfig up and down to get 'hcitool' to work in some cases.
+# Can additionally restart bluetooth services with 'service bluetooth restart'
+# and 'service dbus restart'.
+# Another utility that can be used is 'bluetoothctl' in an interactive session.
